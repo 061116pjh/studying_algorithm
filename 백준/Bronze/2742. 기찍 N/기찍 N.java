848d57cb77d1fac9a3c49ecbd08743c94eee54da
Jong Hyuk Park <105863332+061116pjh@users.noreply.github.com>
2023-06-03 00:56:18 +0900

    [Bronze IV] Title: 기찍 N, Time: 1112 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int startIndex = sc.nextInt();
+		for(int i=startIndex; i>=1; i--) {
+			System.out.println(i);
+		}
+	}
+}
