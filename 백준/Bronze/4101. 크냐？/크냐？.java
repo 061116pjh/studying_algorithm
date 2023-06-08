e2239bec51f15ff92d757fd34e3204f69e34a2de
Jong Hyuk Park <105863332+061116pjh@users.noreply.github.com>
2023-06-09 00:24:00 +0900

    [Bronze V] Title: 크냐?, Time: 232 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			if(A == 0 && B == 0) {
+				break;
+			}else if(A<B) {
+				System.out.println("No");
+			}else if(A == B) {
+				System.out.println("No");
+			}else {
+				System.out.println("Yes");
+			}
+		}
+	}
+
+}
